a052f58965cfcf97c775adaf3de959f63d5e8767
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2022-01-12 18:01:19 +0530

    trying changes for heroku

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import BlogNew from './blogs/BlogNew';
+import BlogShow from './blogs/BlogShow';
 
 class App extends Component {
   componentDidMount() {
@@ -13,7 +19,13 @@ class App extends Component {
       <div className="container">
         <BrowserRouter>
           <div>
-            client
+            <Header />
+            <Switch>
+              <Route path="/blogs/new" component={BlogNew} />
+              <Route exact path="/blogs/:_id" component={BlogShow} />
+              <Route path="/blogs" component={Dashboard} />
+              <Route path="/" component={Landing} />
+            </Switch>
           </div>
         </BrowserRouter>
       </div>
--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-  googleClientID:'70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: "mongodb+srv://SUPERUSER:SUPERUSER@cluster0.33raf.mongodb.net/MARKETPLACE?retryWrites=true&w=majority",
   cookieKey: '123123123',
