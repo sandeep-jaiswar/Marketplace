d0c9e5eb9f05e40e90c59c3421bb35ff138c1d7e
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2022-01-12 16:26:48 +0530

    added scrach code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import BlogNew from './blogs/BlogNew';
+import BlogShow from './blogs/BlogShow';
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +19,13 @@ class App extends Component {
       <div className="container">
         <BrowserRouter>
           <div>
-            hi from react
+            <Header />
+            <Switch>
+              <Route path="/blogs/new" component={BlogNew} />
+              <Route exact path="/blogs/:_id" component={BlogShow} />
+              <Route path="/blogs" component={Dashboard} />
+              <Route path="/" component={Landing} />
+            </Switch>
           </div>
         </BrowserRouter>
       </div>
--- models/Blog.js
+++ models/Blog.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const productSchema = new Schema({
+const blogSchema = new Schema({
   title: String,
   content: String,
   createdAt: { type: Date, default: Date.now },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
-mongoose.model('Product', productSchema);
+mongoose.model('Blog', blogSchema);
